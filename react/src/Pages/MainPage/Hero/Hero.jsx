df6bb27832a36bcf87cf2290868b429c69138c4e
ahmedAhmedHamed <124739282+ahmedAhmedHamed@users.noreply.github.com>
2025-05-05 16:17:50 +0300

    removed skeleton from hero section

--- react/src/Pages/MainPage/Hero/Hero.jsx
+++ react/src/Pages/MainPage/Hero/Hero.jsx
@@ -1,10 +1,8 @@
-import teeth from "@Assets/crowdingTeeth.png";
 import styles from "./Hero.module.css";
-import video from "@Assets/ssyoutube.site - duel of the fates (720p).mp4";
-import Skeleton from "@Assets/skeleton-inside-svgrepo-com.svg";
 import FloatingObject from "@Pages/MainPage/FloatingObject/FloatingObject";
-import FadeInFromRight from "../../../components/FadeInFromRight";
-
+import OBJViewer from "@Components/OBJViewer.jsx";
+import fullOpacity from "@Assets/full_opacity.jpg";
+import transparent from "@Assets/transparent.jpg";
 const Hero = () => {
   return (
     <section className={`${styles.Hero} Container`}>
@@ -13,17 +11,14 @@ const Hero = () => {
         Featuring Segmentations And 3D Vision
       </h1>
       <div className={styles.HeroVideo}>
-        <video src={video} autoPlay muted loop playsInline></video>
+        <OBJViewer plyUrl="/models/combined_fps_pointcloud4_transformed.ply"/>
       </div>
       <div className={styles.Curve}>
         <svg viewBox="0 0 1440 150" preserveAspectRatio="none">
-          <path d="M0,0 C480,100 960,0 1440,100 L1440,150 L0,150 Z" fill="#f5f5f5" />
+          <path d="M0,0 C480,100 960,0 1440,100 L1440,150 L0,150 Z" fill="#f5f5f5"/>
         </svg>
       </div>
-        <FloatingObject
-          src={Skeleton}
-          customStyle={{ width: '700px' }}  // Inline styling for FloatingObject
-        />
+
     </section>
   );
 };
